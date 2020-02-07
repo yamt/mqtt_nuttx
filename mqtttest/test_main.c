666879f8616feceda13676b2c6cf028f18f34d48
YAMAMOTO Takashi <yamamoto@midokura.com>
2020-02-07 12:41:54 +0900

    mqtttest

--- mqtttest/test_main.c
+++ mqtttest/test_main.c
@@ -1,9 +1,24 @@
 
 #include <nuttx/compiler.h>
 
+#include <stdlib.h>
+
+#include <MQTTClient.h>
+
 int
 main(int argc, FAR char *argv[])
 {
+	const char *host;
+	const char *port;
+
+	host = argv[1];
+	host = argv[2];
+
+	MQTTSocket s;
+	MQTTClient c;
+
+	MQTTSocketInit(&s, 0);
+	MQTTSocketConnect(&s, host, port);
 
 	return 0;
 }
